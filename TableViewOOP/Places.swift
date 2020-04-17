789a5e9866f443147d5e9abcc0771b9c5f0ed4d2
YASIN AKCA <yasin.akca@bil.omu.edu.tr>
2020-04-17 17:35:25 +0300

    first commit

---
+++ TableViewOOP/Places.swift
@@ -0,0 +1,9 @@
+//
+//  Places.swift
+//  TableViewOOP
+//
+//  Created by YASIN AKCA on 17.04.2020.
+//  Copyright © 2020 YASIN AKCA. All rights reserved.
+//
+
+import Foundation
---
+++ TableViewOOP/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  TableViewOOP
+//
+//  Created by YASIN AKCA on 17.04.2020.
+//  Copyright © 2020 YASIN AKCA. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
